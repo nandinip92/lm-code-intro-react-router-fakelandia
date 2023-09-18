5b1fcf0df41d873d378d071e055d05586431bfec
nandinip92 <nandini.p8892@gmail.com>
2023-09-18 23:16:04 +0100

    feat: Adding seperate component for the table header

---
+++ client/src/components/misdemeanour/table-header.tsx
@@ -0,0 +1,12 @@
+export const TableHeader = () => (
+  <>
+    <div className="table__row table__row--header">
+      <div className="wrapper">
+        <div className="column citizenId"> Citizen_Id</div>
+        <div className="column date">Date</div>
+        <div className="column misdemeanour">Misdemeanour</div>
+        <div className="column punishmentIdea">Punishment Idea</div>
+      </div>
+    </div>
+  </>
+);
