21756d569362524dd38a14ceb72f61ab37e043c4
nandinip92 <nandini.p8892@gmail.com>
2023-09-19 01:23:12 +0100

    create: Moving <Filter>,<TableHeader> and <TableContent> componets into this seperate component

---
+++ client/src/components/misdemeanour/table.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { useState } from "react";
+import { Misdemeanour } from "../../types/misdemeanour.types";
+import Filter from "./filter";
+import { TableContents } from "./table-contents";
+import { TableHeader } from "./table-header";
+
+interface TableProps {
+  misdemeanours: Array<Misdemeanour>;
+}
+export const Table: React.FC<TableProps> = ({ misdemeanours }) => {
+  const [filter, setFilter] = useState("filter");
+  return (
+    <>
+      <Filter filter={setFilter} />
+      <TableHeader />
+      <TableContents misdemeanours={misdemeanours} filter={filter} />
+    </>
+  );
+};
