b8c7d89f314d17d81729757e6a649cc63e50c06c
nandinip92 <nandini.p8892@gmail.com>
2023-09-22 21:21:35 +0100

    feat: Adding hooks and useStates to validate subjectLine,Reasons and code to enable and disable confess button based on the validity

--- client/src/components/confession/confession.tsx
+++ client/src/components/confession/confession.tsx
@@ -1,14 +1,34 @@
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Subject } from "./subject";
 import { ReasonForContact } from "./reason-for-contact";
 import { TextArea } from "./text-area";
+import { validateSubject, validateReason } from "./validate/validate-fields";
 
-export const Confession = () => {
+export const Confession: React.FC<{ onSubmit?: () => void }> = ({
+  onSubmit,
+}) => {
   const [subjectLine, setSubjectLine] = useState("");
   const [reasonForContact, setReasonForContact] = useState(
     "reason for confession"
   );
+  const [valid, setValid] = useState(false);
   const [textArea, setTextArea] = useState("");
+
+  const validate = () => {
+    const isValidSubject = validateSubject(subjectLine);
+    const isValidResaon = validateReason(reasonForContact);
+    return isValidSubject[0] && isValidResaon[0];
+  };
+
+  useEffect(() => {
+    const isValid = validate();
+    console.log("isValid--->", isValid);
+    setValid(isValid);
+  }, [subjectLine, reasonForContact]);
+
+  const submitForm = () => {
+    onSubmit && onSubmit();
+  };
   return (
     <>
       <h3>
@@ -26,6 +46,16 @@ export const Confession = () => {
           onChangeField={setReasonForContact}
         />
         <TextArea textArea={textArea} onChangeTextArea={setTextArea} />
+        <button
+          className="submit_button"
+          type="submit"
+          title="Submit"
+          value="Confess"
+          onClick={submitForm}
+          disabled={!valid}
+        >
+          Confess
+        </button>
       </div>
     </>
   );
