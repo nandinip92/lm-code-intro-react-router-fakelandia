b165552a02562d8ec7e72a5803fb39cf946279fa
nandinip92 <nandini.p8892@gmail.com>
2023-09-29 14:45:48 +0100

    feat: Adding the count of the misdemeanours to the home page

--- client/src/components/home/home.tsx
+++ client/src/components/home/home.tsx
@@ -1,1 +1,17 @@
-export const Home: React.FC = () => <>Home!</>;
+import { useContext } from "react";
+import { MisdemeanourContext } from "../../types/misdemeanour.types";
+import { misdemeanourListContext } from "../layouts/main_layouts";
+export const Home: React.FC = () => {
+  const { misdemeanours } = useContext(
+    misdemeanourListContext
+  ) as MisdemeanourContext;
+  const confessionsToday = misdemeanours.length - 10;
+  return (
+    <>
+      <h2 className="homePage">Total misdemeanours: {misdemeanours.length}</h2>
+      <h2 className="homePage">
+        Number of confessions recieved today: {confessionsToday}
+      </h2>
+    </>
+  );
+};
