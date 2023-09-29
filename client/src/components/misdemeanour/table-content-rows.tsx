19b5331d4a37fe4b79882a18782140426b46a9eb
nandinip92 <nandini.p8892@gmail.com>
2023-09-29 13:44:17 +0100

    feat: Adding code to hange the self-confessed data from the Confession page and display them accordingly in the misdemeanours page

--- client/src/components/misdemeanour/table-content-rows.tsx
+++ client/src/components/misdemeanour/table-content-rows.tsx
@@ -3,6 +3,40 @@ import { Misdemeanour } from "../../types/misdemeanour.types";
 
 export interface TableContentRows {
   misdemeanour: Misdemeanour;
+  selfConfessed?: boolean; //flag to check if the data is from server or from the Confession page
+}
+export const TableContentRows: React.FC<TableContentRows> = ({
+  misdemeanour,
+  selfConfessed,
+}) => {
+  let rowName = "table__row";
+  if (selfConfessed) rowName = "table__row--selfConfession";
+  return (
+    <>
+      <div className={rowName}>
+        <div className="wrapper">
+          <div className="column citizenId">{misdemeanour.citizenId}</div>
+          <div className="column date">{misdemeanour.date}</div>
+          <div className="column misdemeanour">{misdemeanour.misdemeanour}</div>
+          <div className="column punishmentIdea">
+            <img src="https://picsum.photos/100" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+/*
+********************************************************************************************************************************
+* Following code is used when displaying data fetched from server onto the misdemeanours page.                                 *
+* Handlining the self Confessed data on the Confession page from client-side as of now, so commenting this code for time being *
+********************************************************************************************************************************
+import React from "react";
+import { Misdemeanour } from "../../types/misdemeanour.types";
+
+export interface TableContentRows {
+  misdemeanour: Misdemeanour;
 }
 export const TableContentRows: React.FC<TableContentRows> = ({
   misdemeanour,
@@ -22,3 +56,4 @@ export const TableContentRows: React.FC<TableContentRows> = ({
     </>
   );
 };
+*/
--- client/src/components/misdemeanour/table-contents.tsx
+++ client/src/components/misdemeanour/table-contents.tsx
@@ -2,8 +2,59 @@ import React from "react";
 import { useContext } from "react";
 import { MisdemeanourContext } from "../../types/misdemeanour.types";
 import { TableContentRows } from "./table-content-rows";
-import { misdemeanourListContext } from "../router/router";
 import { v4 as uuidv4 } from "uuid";
+import { misdemeanourListContext } from "../layouts/main_layouts";
+
+export interface TableContentsProps {
+  filter: string;
+}
+
+export const TableContents: React.FC<TableContentsProps> = ({ filter }) => {
+  const { misdemeanours } = useContext(
+    misdemeanourListContext
+  ) as MisdemeanourContext;
+  //Since we are pulling 10 records from the server initially, the rest of the records are considered as self-confessed data from the confessions page
+  const selfConfessedMisdemeanour = misdemeanours
+    .slice(10)
+    .map((misdeed) => misdeed.citizenId);
+  const records =
+    filter !== "no_filter"
+      ? misdemeanours.filter((m) => m.misdemeanour === filter)
+      : misdemeanours;
+
+  //'selfConfessed' an optional attribute to differentiate between the self-confessed data recieved from the confession page and the data retrieved from the server
+  return (
+    <>
+      <div className="misdeedsTable__contents">
+        {records.map((misdemeanour) =>
+          selfConfessedMisdemeanour.includes(misdemeanour.citizenId) ? (
+            <TableContentRows
+              key={uuidv4()}
+              misdemeanour={misdemeanour}
+              selfConfessed={true}
+            />
+          ) : (
+            <TableContentRows key={uuidv4()} misdemeanour={misdemeanour} />
+          )
+        )}
+      </div>
+    </>
+  );
+};
+
+/*
+
+********************************************************************************************************************************
+* Following code is used when displaying data fetched from server onto the misdemeanours page.                                 *
+* Handlining the self Confessed data on the Confession page from client-side as of now, so commenting this code for time being *
+********************************************************************************************************************************
+import React from "react";
+import { useContext } from "react";
+import { MisdemeanourContext } from "../../types/misdemeanour.types";
+import { TableContentRows } from "./table-content-rows";
+import { v4 as uuidv4 } from "uuid";
+//import { misdemeanourListContext } from "../router/router";
+import { misdemeanourListContext } from "../layouts/main_layouts";
 
 export interface TableContentsProps {
   filter: string;
@@ -14,7 +65,6 @@ export const TableContents: React.FC<TableContentsProps> = ({ filter }) => {
   const { misdemeanours } = useContext(
     misdemeanourListContext
   ) as MisdemeanourContext;
-
   const records =
     filter !== "filter"
       ? misdemeanours.filter((m) => m.misdemeanour === filter)
@@ -29,3 +79,4 @@ export const TableContents: React.FC<TableContentsProps> = ({ filter }) => {
     </>
   );
 };
+*/
