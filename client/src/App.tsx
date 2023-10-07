25c93abf5af5a93827e507ea199e44be82b68e4c
nandinip92 <nandini.p8892@gmail.com>
2023-10-07 16:26:34 +0100

    refactor: replacing <BrowserRouter> into <HashRouter>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,12 @@
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Router } from "./components/router/router";
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Router />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
