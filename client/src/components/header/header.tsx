4508d3c8b97d6b5722fa7c4c604d09b5dac9ed27
nandinip92 <nandini.p8892@gmail.com>
2023-10-07 15:56:14 +0100

    refactor: adding an emoji to the header

--- client/src/components/header/header.tsx
+++ client/src/components/header/header.tsx
@@ -1,7 +1,9 @@
 import { Nav } from "./nav";
 const Header = () => (
   <header className="fakelandiaHeader">
-    <h1 className="fakelandiaHeader__title">FAKELANDIA JUSTICE DEPARTMENT</h1>
+    <h1 className="fakelandiaHeader__title">
+      ⚖️ FAKELANDIA JUSTICE DEPARTMENT
+    </h1>
     <Nav />
   </header>
 );
