fbe5780d639ff690f4384d63a7f0558bfc868c05
nandinip92 <nandini.p8892@gmail.com>
2023-10-07 15:12:32 +0100

    refactor: Adding styles to the home page

--- client/src/components/home/home.tsx
+++ client/src/components/home/home.tsx
@@ -5,10 +5,18 @@ export const Home: React.FC = () => {
   const confessionsToday = misdemeanours.length - 10;
   return (
     <>
-      <h2 className="homePage">Total misdemeanours: {misdemeanours.length}</h2>
-      <h2 className="homePage">
-        Number of confessions recieved today: {confessionsToday}
-      </h2>
+      <div className="cards__wrapper">
+        <div className="card">
+          <p className="card__text">
+            Total misdemeanours: {misdemeanours.length}
+          </p>
+        </div>
+        <div className="card">
+          <p className="card__text">
+            Number of confessions recieved today: {confessionsToday}
+          </p>
+        </div>
+      </div>
     </>
   );
 };
