4c5d388d9e2f154384a6b0af26ee550ed5336d81
nandinip92 <nandini.p8892@gmail.com>
2023-10-07 16:15:18 +0100

    refactor: Adding HashRouter

--- client/src/components/router/router.tsx
+++ client/src/components/router/router.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { MainLayout } from "../layouts/main_layouts";
 import { Home } from "../home/home";
 import { MisdemeanourPage } from "../misdemeanour/misdemeanour-page";
@@ -6,14 +6,16 @@ import { Confession } from "../confession/confession";
 import NotFound from "../not_found/not_found";
 
 export const Router = () => (
-  <Routes>
-    <Route path="/" element={<MainLayout />}>
-      <Route index element={<Home />} />
-      <Route path="/misdemeanour" element={<MisdemeanourPage />} />
-      <Route path="/confession" element={<Confession />} />
-      <Route path="*" element={<NotFound />} />
-    </Route>
-  </Routes>
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Home />} />
+        <Route path="/misdemeanour" element={<MisdemeanourPage />} />
+        <Route path="/confession" element={<Confession />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  </HashRouter>
 );
 
 /* 
