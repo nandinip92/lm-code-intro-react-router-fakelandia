45d50887c5ba8445204023075cc8e7f3449444ae
nandinip92 <nandini.p8892@gmail.com>
2023-10-09 15:10:57 +0100

    chore: Deploying into gh-pages

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/lm-code-intro-react-router-fakelandia/",
 });
